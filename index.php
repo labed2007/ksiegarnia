22b4648dca3d869682e6567c9f4c07e287c8533a
Paweł Łabędzki <labed92@gmail.com>
2015-07-16 10:40:54 +0200

    added form in index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
+<Html>
+<body>
+
+
+<form action="" method="post">
+
+    <div>
+
+        booktitle:<br/>
+
+        <input name="bookTitle" value=""/><br/>
+
+        <input type="submit" value="Send" name="submit"/>
+    </div>
+</form>
+
 <?php
-include classes/domian/book.php;
-include classes/repository/bookRepository.php;
+include classes / domian / book . php;
+include classes / repository / bookRepository . php;
 ?>
+
+
+</body>
+</Html>
